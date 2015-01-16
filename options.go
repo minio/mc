8696850601378843fb069dd531b525cda27560e6
Harshavardhana <harsha@harshavardhana.net>
2015-01-15 18:45:25 -0800

    Implement new options

--- options.go
+++ options.go
@@ -17,42 +17,112 @@
 package main
 
 import (
+	"log"
+	"os"
+	"strings"
+
 	"github.com/codegangsta/cli"
-	//	"github.com/minio-io/mc/pkg/s3"
+	"github.com/minio-io/mc/pkg/s3"
 )
 
+type MinioClient struct {
+	bucketName string
+	keyName    string
+	body       string
+	bucketAcls string
+	policy     string
+	region     string
+	query      string // TODO
+}
+
 var Options = []cli.Command{
-	Get,
-	Put,
-	List,
+	GetObject,
+	PutObject,
+	ListObjects,
+	ListBuckets,
+	Configure,
 }
 
-var Get = cli.Command{
-	Name:        "get",
+var GetObject = cli.Command{
+	Name:        "get-object",
 	Usage:       "",
 	Description: "",
-	Action:      doGet,
+	Action:      doGetObject,
 }
 
-var Put = cli.Command{
-	Name:        "put",
+var PutObject = cli.Command{
+	Name:        "put-object",
 	Usage:       "",
 	Description: "",
-	Action:      doPut,
+	Action:      doPutObject,
 }
 
-var List = cli.Command{
-	Name:        "list",
+var ListObjects = cli.Command{
+	Name:        "list-objects",
 	Usage:       "",
 	Description: "",
-	Action:      doList,
+	Action:      doListObjects,
 }
 
-func doGet(c *cli.Context) {
+var ListBuckets = cli.Command{
+	Name:        "list-buckets",
+	Usage:       "",
+	Description: "",
+	Action:      doListBuckets,
 }
 
-func doPut(c *cli.Context) {
+var Configure = cli.Command{
+	Name:        "configure",
+	Usage:       "",
+	Description: "",
+	Action:      doConfigure,
 }
 
-func doList(c *cli.Context) {
+func parseInput(c *cli.Context) string {
+	var commandName string
+	switch len(c.Args()) {
+	case 1:
+		commandName = c.Args()[0]
+	default:
+		log.Fatal("command name must not be blank\n")
+	}
+
+	var inputOptions []string
+	if c.String("bucket") != "" {
+		inputOptions = strings.Split(c.String("options"), ",")
+	}
+
+	if inputOptions[0] == "" {
+		log.Fatal("options cannot be empty with a command name")
+	}
+	return commandName
+}
+
+func doGetObject(c *cli.Context) {
+	var bucket, key string
+	accessKey := os.Getenv("AWS_ACCESS_KEY_ID")
+	secretKey := os.Getenv("AWS_ACCESS_SECRET_KEY")
+	if accessKey == "" || secretKey == "" {
+		log.Fatal("no AWS_ACCESS_KEY_ID or AWS_ACCESS_KEY_SECRET set in environment")
+	}
+	s3c := s3.NewS3Client(accessKey, secretKey)
+	_, _, err := s3c.Get(bucket, key)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func doPutObject(c *cli.Context) {
+}
+
+func doListObject(c *cli.Context) {
+}
+
+func doListObjects(c *cli.Context) {
+}
+
+func doListBuckets(c *cli.Context) {
+}
+
+func doConfigure(c *cli.Context) {
 }
