71cdfe5d6fa01495095d4d3d0f473cf294581f36
Harshavardhana <harsha@harshavardhana.net>
2015-03-25 13:13:15 -0700

    Build fix

--- pkg/client/donut/donut.go
+++ pkg/client/donut/donut.go
@@ -56,6 +56,7 @@ func IsValidBucketName(bucket string) bool {
 	return match
 }
 
+// GetNewClient returns an initialized donut driver
 func GetNewClient(path string) client.Client {
 	var err error
 	d := new(donutDriver)
