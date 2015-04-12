20c83f53ce620ce326c3f3ea654ac7f03b8ea616
Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
2015-04-12 11:35:39 -0700

    Fix build

--- pkg/client/s3/object.go
+++ pkg/client/s3/object.go
@@ -100,19 +100,23 @@ func (c *s3Client) Put(bucket, key, md5HexString string, size int64) (io.WriteCl
 	return blockingWriter, nil
 }
 
-type blockingWriteCloser struct {
+// BlockingWriteCloser is a WriteCloser that blocks until released
+type BlockingWriteCloser struct {
 	w       io.WriteCloser
 	release *sync.WaitGroup
 	err     error
 }
 
-func (b *blockingWriteCloser) Write(p []byte) (int, error) {
+// Write to the underlying writer
+func (b *BlockingWriteCloser) Write(p []byte) (int, error) {
 	n, err := b.w.Write(p)
 	err = iodine.New(err, nil)
 	return n, err
 }
 
-func (b *blockingWriteCloser) Close() error {
+// Close blocks until another goroutine calls Release(error). Returns error code if either
+// writer fails or Release is called with an error.
+func (b *BlockingWriteCloser) Close() error {
 	err := b.w.Close()
 	if err != nil {
 		b.err = err
@@ -121,17 +125,19 @@ func (b *blockingWriteCloser) Close() error {
 	return b.err
 }
 
-func (b *blockingWriteCloser) Release(err error) {
+// Release the Close, causing it to unblock. Only call this once. Calling it multiple times results in a panic.
+func (b *BlockingWriteCloser) Release(err error) {
 	b.release.Done()
 	if err != nil {
 		b.err = err
 	}
 }
 
-func NewBlockingWriteCloser(w io.WriteCloser) *blockingWriteCloser {
+// NewBlockingWriteCloser Creates a new write closer that must be released by the read consumer.
+func NewBlockingWriteCloser(w io.WriteCloser) *BlockingWriteCloser {
 	wg := &sync.WaitGroup{}
 	wg.Add(1)
-	return &blockingWriteCloser{w: w, release: wg}
+	return &BlockingWriteCloser{w: w, release: wg}
 }
 
 // Stat - returns 0, "", os.ErrNotExist if not on S3
