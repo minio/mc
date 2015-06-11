8befc50050c5af08b1df1478aeeac32e1286df0f
Harshavardhana <harsha@minio.io>
2015-06-10 18:34:46 -0700

    Update docs/* remove stale documentation

--- cmd-sync.go
+++ cmd-sync.go
@@ -53,7 +53,7 @@ EXAMPLES:
       $ mc {{.Name}} star-trek-episode-10-season4.ogg https://s3.amazonaws.com/trekarchive
 
    2. Sync a bucket recursively from Minio object storage to multiple buckets on Amazon S3 object storage.
-      $ mc {{.Name}} https://play.minio.io:9000/photos/2014... https://s3.amazonaws.com/backup-photos https://s3.amazonaws.com/my-photos
+      $ mc {{.Name}} https://play.minio.io:9000/photos/2014... https://s3.amazonaws.com/backup-photos https://s3-west-1.amazonaws.com/local-photos
 
    3. Sync a local folder recursively to Minio object storage and Amazon S3 object storage.
       $ mc {{.Name}} backup/... https://play.minio.io:9000/archive https://s3.amazonaws.com/archive
@@ -61,8 +61,8 @@ EXAMPLES:
    4. Sync a bucket from aliased Amazon S3 object storage to multiple folders on Windows.
       $ mc {{.Name}} s3:documents/2014/... C:\backup\2014 C:\shared\volume\backup\2014
 
-   5. Sync a local file of non english character to Amazon s3 object storage.
-      $ mc {{.Name}} 本語/... s3:mylocaldocuments C:\backup\2014 play:backup
+   5. Sync a local directory of non english character recursively to Amazon s3 object storage and Minio object storage.
+      $ mc {{.Name}} 本語/... s3:mylocaldocuments play:backup
 
 `,
 }
