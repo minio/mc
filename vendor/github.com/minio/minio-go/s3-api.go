2ea16262e02ff9a6264624fb7e1c21c59676a2f1
Krishna Srinivas <krishna.srinivas@gmail.com>
2015-11-13 19:55:26 -0800

    Fix to use single connection to the server

--- vendor/github.com/minio/minio-go/s3-api.go
+++ vendor/github.com/minio/minio-go/s3-api.go
@@ -41,7 +41,15 @@ type s3API struct {
 
 // closeResp close non nil response with any response Body.
 func closeResp(resp *http.Response) {
+	// Callers should close resp.Body when done reading from it.
+	// If resp.Body is not closed, the Client's underlying RoundTripper
+	// (typically Transport) may not be able to re-use a persistent TCP
+	// connection to the server for a subsequent "keep-alive" request.
 	if resp != nil && resp.Body != nil {
+		// Drain any remaining Body and then close the connection.
+		// Without this closing connection would disallow re-using
+		// the same connection for future uses.
+		//  - http://stackoverflow.com/a/17961593/4465767
 		io.Copy(ioutil.Discard, resp.Body)
 		resp.Body.Close()
 	}
