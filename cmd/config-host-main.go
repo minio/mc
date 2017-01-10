5aac31c4bd00ab298c263670177e4835348647ca
P Krishnamoorthy <k.poorna@gmail.com>
2017-01-09 22:59:34 -0800

    sort by alias

--- cmd/config-host-main.go
+++ cmd/config-host-main.go
@@ -19,6 +19,7 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/fatih/color"
@@ -265,6 +266,13 @@ func removeHost(alias string) {
 	printMsg(hostMessage{op: "remove", Alias: alias})
 }
 
+//byAlias is a collection satisfying sort.Interface
+type byAlias []hostMessage
+
+func (d byAlias) Len() int           { return len(d) }
+func (d byAlias) Swap(i, j int)      { d[i], d[j] = d[j], d[i] }
+func (d byAlias) Less(i, j int) bool { return d[i].Alias < d[j].Alias }
+
 // listHosts - list all host URLs.
 func listHosts() {
 	conf, err := loadMcConfig()
@@ -275,14 +283,22 @@ func listHosts() {
 			maxAlias = len(k)
 		}
 	}
+	var hosts []hostMessage
+	sort.Sort(byAlias(hosts))
 	for k, v := range conf.Hosts {
-		printMsg(hostMessage{
+
+		hosts = append(hosts, hostMessage{
 			op:        "list",
-			Alias:     fmt.Sprintf("%*.*s", maxAlias, maxAlias, k),
+			Alias:     fmt.Sprintf("%-*.*s", maxAlias, maxAlias, k),
 			URL:       v.URL,
 			AccessKey: v.AccessKey,
 			SecretKey: v.SecretKey,
 			API:       v.API,
 		})
+	}
+	sort.Sort(byAlias(hosts))
+	for _, host := range hosts {
+		host.Alias = fmt.Sprintf("%-*.*s", maxAlias, maxAlias, host.Alias)
+		printMsg(host)
 	}
 }
