8f38471e299a3b29cbfa905e5c238039fb0d4eb7
A. Elleuch <vadmeste@users.noreply.github.com>
2017-10-06 17:35:44 +0100

    fix: Correct some cp/mirror help indentations (#2268)

--- cmd/mirror-main.go
+++ cmd/mirror-main.go
@@ -78,9 +78,9 @@ USAGE:
 
 FLAGS:
   {{range .VisibleFlags}}{{.}}
-	{{end}}
+  {{end}}
 ENVIRONMENT VARIABLES:
-	MC_MULTIPART_THREADS: To set number of multipart threads. By default it is 4.
+  MC_MULTIPART_THREADS: To set number of multipart threads. By default it is 4.
 
 EXAMPLES:
    1. Mirror a bucket recursively from Minio cloud storage to a bucket on Amazon S3 cloud storage.
