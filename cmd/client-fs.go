4e7eeb0725abcd36b54a37636ff89f29c3238fd8
Praveen raj Mani <praveen@minio.io>
2018-12-22 03:08:12 +0530

    Ignore broken symlink durring mirror (#2607)
    Fixes #2604

--- cmd/client-fs.go
+++ cmd/client-fs.go
@@ -624,7 +624,8 @@ func (f *fsClient) listInRoutine(contentCh chan<- *clientContent) {
 		for _, file := range files {
 			fi := file
 			if fi.Mode()&os.ModeSymlink == os.ModeSymlink {
-				fi, e = os.Stat(filepath.Join(fpath, fi.Name()))
+				fp := filepath.Join(fpath, fi.Name())
+				fi, e = os.Stat(fp)
 				if os.IsPermission(e) {
 					contentCh <- &clientContent{
 						Err: probe.NewError(PathInsufficientPermission{Path: pathURL.Path}),
@@ -632,8 +633,12 @@ func (f *fsClient) listInRoutine(contentCh chan<- *clientContent) {
 					continue
 				}
 				if os.IsNotExist(e) {
+					// Lstat makes no attempt to follow the broken link.
+					_, e = os.Lstat(fp)
 					contentCh <- &clientContent{
-						Err: probe.NewError(BrokenSymlink{Path: file.Name()}),
+						URL:  pathURL,
+						Size: -1,
+						Err:  probe.NewError(e),
 					}
 					continue
 				}
@@ -810,10 +815,13 @@ func (f *fsClient) listRecursiveInRoutine(contentCh chan *clientContent) {
 					}
 					return nil
 				}
-				// Ignore in-accessible broken symlinks.
 				if os.IsNotExist(e) {
+					// Lstat makes no attempt to follow the broken link.
+					_, e = os.Lstat(fp)
 					contentCh <- &clientContent{
-						Err: probe.NewError(BrokenSymlink{Path: fp}),
+						URL:  *newClientURL(fp),
+						Size: -1,
+						Err:  probe.NewError(e),
 					}
 					return nil
 				}
