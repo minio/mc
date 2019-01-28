5eb9eeb2954881c77c7310f71a6edb7eb4511bc8
wlan0 <sidharthamn@gmail.com>
2019-01-28 01:39:13 +0000

    Start using Apache V2 licensed fork of golang's encoding/json

--- cmd/admin-monitor.go
+++ cmd/admin-monitor.go
@@ -1,7 +1,22 @@
+/*
+ * Minio Client (C) 2016, 2017 Minio, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package cmd
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/url"
 	"sort"
@@ -11,6 +26,7 @@ import (
 	"github.com/fatih/color"
 	"github.com/minio/cli"
 	"github.com/minio/mc/pkg/console"
+	"github.com/minio/mc/pkg/json"
 	"github.com/minio/mc/pkg/probe"
 	"github.com/minio/minio/pkg/madmin"
 )
