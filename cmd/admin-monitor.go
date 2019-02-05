7bc792e7454230c5af410774558f3dd996a4787d
Sidhartha Mani <wlan0@users.noreply.github.com>
2019-02-04 22:22:33 -0800

    Add json pretty print (#2661)
    Enable jq style color output in github.com/minio/mc/pkg/json
    - Drop in replacement of encoding/json
    - Bold struct keys in blue
    - Strings in green
    - Numbers in default color
    - Indenting and spacing matches exact jq specifications
    
    Additionally, color coding needs '\x1b[...m' to be whitelisted in encoding/json's parser

--- cmd/admin-monitor.go
+++ cmd/admin-monitor.go
@@ -1,7 +1,22 @@
+/*
+ * Minio Client (C) 2019 Minio, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package cmd
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/url"
 	"sort"
@@ -10,6 +25,7 @@ import (
 	humanize "github.com/dustin/go-humanize"
 	"github.com/fatih/color"
 	"github.com/minio/cli"
+	json "github.com/minio/mc/pkg/colorjson"
 	"github.com/minio/mc/pkg/console"
 	"github.com/minio/mc/pkg/probe"
 	"github.com/minio/minio/pkg/madmin"
@@ -27,7 +43,7 @@ const (
 
 var adminMonitorCmd = cli.Command{
 	Name:            "monitor",
-	Usage:           "monitor cpu, mem statistics",
+	Usage:           "monitor cpu and mem statistics",
 	Action:          mainAdminMonitor,
 	Before:          setGlobalsFromContext,
 	Flags:           append(adminMonitorFlags, globalFlags...),
@@ -42,7 +58,7 @@ FLAGS:
   {{range .VisibleFlags}}{{.}}
   {{end}}
 EXAMPLES:
-  1. Get server cpu, mem statistics of the 'play' server.
+  1. Get server cpu and mem statistics of the 'play' server.
        $ {{.HelpName}} play/
 `,
 }
