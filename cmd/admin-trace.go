64d45ac35a34ea76aa6d73659ca698390960c237
Sidhartha Mani <wlan0@users.noreply.github.com>
2019-08-16 16:57:40 -0700

    fix formatting for admin trace (#2854)

--- cmd/admin-trace.go
+++ cmd/admin-trace.go
@@ -222,7 +222,7 @@ func (s shortTraceMsg) String() string {
 	}
 	fmt.Fprintf(b, "%s ", s.Time.Format(timeFormat))
 	statusStr := console.Colorize("RespStatus", fmt.Sprintf("%d %s", s.StatusCode, s.StatusMsg))
-	if s.StatusCode != http.StatusOK {
+	if s.StatusCode >= http.StatusBadRequest {
 		statusStr = console.Colorize("ErrStatus", fmt.Sprintf("%d %s", s.StatusCode, s.StatusMsg))
 	}
 	fmt.Fprintf(b, "[%s] %s ", statusStr, console.Colorize("FuncName", s.FuncName))
@@ -232,11 +232,15 @@ func (s shortTraceMsg) String() string {
 		fmt.Fprintf(b, "?%s ", s.Query)
 	}
 	fmt.Fprintf(b, " %s ", s.Client)
-	fmt.Fprint(b, console.Colorize("HeaderValue", fmt.Sprintf("%2s ", s.CallStats.Duration.Round(time.Microsecond))))
-	fmt.Fprint(b, console.Colorize("Stat", fmt.Sprintf(" 🠉 ")))
-	fmt.Fprint(b, console.Colorize("HeaderValue", humanize.IBytes(uint64(s.CallStats.Rx))))
-	fmt.Fprint(b, console.Colorize("Stat", fmt.Sprintf("  🠋 ")))
-	fmt.Fprint(b, console.Colorize("HeaderValue", humanize.IBytes(uint64(s.CallStats.Tx))))
+	spaces := 15 - len(s.Client)
+	fmt.Fprintf(b, "%*s", spaces, " ")
+	fmt.Fprintf(b, console.Colorize("HeaderValue", fmt.Sprintf("  %2s", s.CallStats.Duration.Round(time.Microsecond))))
+	spaces = 12 - len(fmt.Sprintf("%2s", s.CallStats.Duration.Round(time.Microsecond)))
+	fmt.Fprintf(b, "%*s", spaces, " ")
+	fmt.Fprintf(b, console.Colorize("Stat", fmt.Sprintf(" 🠉 ")))
+	fmt.Fprintf(b, console.Colorize("HeaderValue", humanize.IBytes(uint64(s.CallStats.Rx))))
+	fmt.Fprintf(b, console.Colorize("Stat", fmt.Sprintf("  🠋 ")))
+	fmt.Fprintf(b, console.Colorize("HeaderValue", humanize.IBytes(uint64(s.CallStats.Tx))))
 
 	return b.String()
 }
