67e90b8fcbc9781fa3bf2d183cd457c2e5267f81
Harshavardhana <harsha@minio.io>
2020-02-25 18:08:20 +0530

    fix handling of directories in Stat (#3092)
    Fixes #3089
    Fixes #3091

--- cmd/client-s3.go
+++ cmd/client-s3.go
@@ -1287,7 +1287,7 @@ func (c *s3Client) Stat(isIncomplete, isFetchMeta, isPreserve bool, sse encrypt.
 		if objectStat.Err != nil {
 			return nil, probe.NewError(objectStat.Err)
 		}
-		if strings.HasSuffix(prefix, string(c.targetURL.Separator)) {
+		if strings.HasSuffix(objectStat.Key, string(c.targetURL.Separator)) {
 			objectMetadata.URL = *c.targetURL
 			objectMetadata.Type = os.ModeDir
 			if isFetchMeta {
