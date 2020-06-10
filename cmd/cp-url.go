minio/mc

commit fd43df09a1c622903f6e872eab390f377b1ed400

mc cp: Fix prefix handling in TypeC (#3115)
for copy from filesystem to objectstorage.

Avoid prefixing dirname to file on target side
when doing a `mc cp /tmp/dir myminio/bucket --r`
to make `mc mirror` and `mc cp` behavior consistent

cmd/cp-url.go
@@ -191,6 +191,11 @@ func makeCopyContentTypeC(sourceAlias string, sourceURL clientURL, sourceContent
 	newSourceSuffix := filepath.ToSlash(newSourceURL.Path)
 	if pathSeparatorIndex > 1 {
 		sourcePrefix := filepath.ToSlash(sourceURL.Path[:pathSeparatorIndex])
+		// do not preserve unix cp behavior when copying from filesytem to
+		// objectstore.
+		if sourceAlias == "" && targetAlias != "" {
+			sourcePrefix = sourceURL.Path
+		}
 		newSourceSuffix = strings.TrimPrefix(newSourceSuffix, sourcePrefix)
 	}
 	newTargetURL := urlJoinPath(targetURL, newSourceSuffix)

commit 145dc50efcd81426de135f91e5715ef37f0e28f1

cp: Fix Windows paths
Paths without `/` separator were not trimmed correctly.

Fixes #3241

Regression originating from #3115

cmd/cp-url.go
@@ -201,7 +201,7 @@ func makeCopyContentTypeC(sourceAlias string, sourceURL ClientURL, sourceContent
 				return URLs{Error: probe.NewError(err)}
 			}
 			if fileInfo.IsDir() {
-				sourcePrefix = sourceURL.Path
+				sourcePrefix = filepath.ToSlash(sourceURL.Path)
 			}
 		}
 		newSourceSuffix = strings.TrimPrefix(newSourceSuffix, sourcePrefix)