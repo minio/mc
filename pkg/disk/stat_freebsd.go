0d5acd882067d94b5ac2ec964c458f8f6547d48c
Klaus Post <klauspost@gmail.com>
2022-03-01 10:23:05 -0800

    Fix arm FreeBSD build (#4006)
    Fixes #4005
    
    Tested with `GOOS=freebsd GOARCH=arm go build .`.

--- pkg/disk/stat_freebsd.go
+++ pkg/disk/stat_freebsd.go
@@ -35,7 +35,8 @@ func GetFileSystemAttrs(file string) (string, error) {
 
 	var fileAttr strings.Builder
 	fileAttr.WriteString("atime:")
-	fileAttr.WriteString(strconv.FormatInt(st.Atimespec.Sec, 10) + "#" + strconv.FormatInt(st.Atimespec.Nsec, 10))
+	// NOTE: Keep int64 casts. See https://github.com/minio/mc/issues/4005
+	fileAttr.WriteString(strconv.FormatInt(int64(st.Atimespec.Sec), 10) + "#" + strconv.FormatInt(int64(st.Atimespec.Nsec), 10))
 	fileAttr.WriteString("/gid:")
 	fileAttr.WriteString(strconv.Itoa(int(st.Gid)))
 
@@ -48,7 +49,8 @@ func GetFileSystemAttrs(file string) (string, error) {
 	fileAttr.WriteString("/mode:")
 	fileAttr.WriteString(strconv.Itoa(int(st.Mode)))
 	fileAttr.WriteString("/mtime:")
-	fileAttr.WriteString(strconv.FormatInt(st.Mtimespec.Sec, 10) + "#" + strconv.FormatInt(st.Mtimespec.Nsec, 10))
+	// NOTE: Keep int64 casts. See https://github.com/minio/mc/issues/4005
+	fileAttr.WriteString(strconv.FormatInt(int64(st.Mtimespec.Sec), 10) + "#" + strconv.FormatInt(int64(st.Mtimespec.Nsec), 10))
 	fileAttr.WriteString("/uid:")
 	fileAttr.WriteString(strconv.Itoa(int(st.Uid)))
 
