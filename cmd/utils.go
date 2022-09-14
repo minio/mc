b34681d662f1a132c8538a27ce1ab65abab613a5
Harshavardhana <harsha@minio.io>
2022-09-14 08:15:37 -0700

    fix: use deadlineconn properly (#4249)
    PR #4164 didn't use the set deadlines properly

--- cmd/utils.go
+++ cmd/utils.go
@@ -144,6 +144,8 @@ func NewS3Config(urlStr string, aliasCfg *aliasConfigV10) *Config {
 	s3Config.AppVersion = ReleaseTag
 	s3Config.Debug = globalDebug
 	s3Config.Insecure = globalInsecure
+	s3Config.ConnReadDeadline = globalConnReadDeadline
+	s3Config.ConnWriteDeadline = globalConnWriteDeadline
 
 	s3Config.HostURL = urlStr
 	if aliasCfg != nil {
