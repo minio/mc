960776b3221e1bc6cac3c2f49d52a3cad7fb8dbc
adfost <adfost@comcast.net>
2022-11-02 17:42:32 -0700

    saving

--- cmd/ilm/parse.go
+++ cmd/ilm/parse.go
@@ -81,8 +81,10 @@ func validateRuleAction(rule lifecycle.Rule) error {
 	expirySet := !rule.Expiration.IsNull()
 	transitionSet := !rule.Transition.IsNull()
 	noncurrentExpirySet := !rule.NoncurrentVersionExpiration.IsDaysNull()
+	newerNoncurrentVersionsExpiry := rule.NoncurrentVersionExpiration.NewerNoncurrentVersions == 0
 	noncurrentTransitionSet := rule.NoncurrentVersionTransition.StorageClass != ""
-	if !expirySet && !transitionSet && !noncurrentExpirySet && !noncurrentTransitionSet {
+	newerNoncurrentVersionsTransition := rule.NoncurrentVersionTransition.NewerNoncurrentVersions == 0
+	if !expirySet && !transitionSet && !noncurrentExpirySet && !noncurrentTransitionSet && !newerNoncurrentVersionsExpiry && !newerNoncurrentVersionsTransition  {
 		return errors.New("at least one of Expiry, Transition, NoncurrentExpiry, NoncurrentVersionTransition actions should be specified in a rule")
 	}
 	return nil
