ec67a511851227680a4e998c39fe14b9802448db
adfost <adfost@comcast.net>
2022-11-07 15:47:23 -0800

    Replicate-update instead of replicate-edit (#4346)

--- cmd/replicate-main.go
+++ cmd/replicate-main.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2021 MinIO, Inc.
+// Copyright (c) 2015-2022 MinIO, Inc.
 //
 // This file is part of MinIO Object Storage stack
 //
@@ -21,7 +21,7 @@ import "github.com/minio/cli"
 
 var replicateSubcommands = []cli.Command{
 	replicateAddCmd,
-	replicateEditCmd,
+	replicateUpdateCmd,
 	replicateListCmd,
 	replicateStatusCmd,
 	replicateResyncCmd,
