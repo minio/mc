9b36e418722d196f654e2fe2f13110639b8c3928
Kanagaraj M <kanagaraj@minio.io>
2023-01-27 11:15:00 +0530

    Use --api-key flag on register when provided

--- cmd/license-register.go
+++ cmd/license-register.go
@@ -170,12 +170,20 @@ func mainLicenseRegister(ctx *cli.Context) error {
 	regInfo := getClusterRegInfo(getAdminInfo(aliasedURL), clusterName)
 
 	alreadyRegistered := false
-	apiKey, _, e := getSubnetCreds(alias)
-	fatalIf(probe.NewError(e), "Error in fetching subnet API Key")
-	if len(apiKey) > 0 {
-		alreadyRegistered = true
-		if len(accAPIKey) == 0 {
-			accAPIKey = apiKey
+	if len(accAPIKey) == 0 {
+		apiKey, _, e := getSubnetCreds(alias)
+		fatalIf(probe.NewError(e), "Error in fetching subnet API Key")
+		if len(apiKey) > 0 {
+			alreadyRegistered = true
+			if len(accAPIKey) == 0 {
+				accAPIKey = apiKey
+			}
+		}
+	} else {
+		apiKey := getSubnetAPIKeyFromConfig(alias)
+		lic := getSubnetLicenseFromConfig(alias)
+		if len(apiKey) > 0 || len(lic) > 0 {
+			alreadyRegistered = true
 		}
 	}
 
@@ -189,7 +197,7 @@ func mainLicenseRegister(ctx *cli.Context) error {
 		lrm.URL = subnetOfflineRegisterURL(regToken)
 	} else {
 		lrm.Type = "online"
-		_, _, e = registerClusterOnSubnet(regInfo, alias, accAPIKey)
+		_, _, e := registerClusterOnSubnet(regInfo, alias, accAPIKey)
 		fatalIf(probe.NewError(e), "Could not register cluster with SUBNET:")
 
 		lrm.Action = "registered"
