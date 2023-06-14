aaef6a75ddced12f1499d34749d2debb574e202b
r-scheele <abdulrahmanolamilekan88@gmail.com>
2023-06-14 01:26:47 -0700

    [updates] - removed commented func

--- cmd/pipe_unsupported.go
+++ cmd/pipe_unsupported.go
@@ -21,11 +21,6 @@ package cmd
 
 import "os"
 
-// func increasePipeBufferSize(f *os.File, desiredPipeSize int) error {
-// 	// this is not supported on non-Linux platforms.
-// 	return nil
-// }
-
 func increasePipeBufferSize(_ *os.File, _ int) error {
 	// this is not supported on non-Linux platforms.
 	return nil
