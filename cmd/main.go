092be48b3328e7bff4a5d8f6282e005a740d8462
Klaus Post <klauspost@gmail.com>
2023-10-17 17:54:47 +0200

    typo

--- cmd/main.go
+++ cmd/main.go
@@ -116,7 +116,7 @@ func Main(args []string) error {
 	probe.SetAppInfo("Commit", ShortCommitID)
 
 	// Fetch terminal size, if not available, automatically
-	// set globalQuiet to on non-window.
+	// set globalQuiet to true on non-window.
 	if w, h, e := term.GetSize(int(os.Stdin.Fd())); e != nil {
 		globalQuiet = runtime.GOOS != "windows"
 	} else {
