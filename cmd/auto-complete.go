c1f61a1e066c50e836377a5ff8130308776422b5
Taran Pelkey <taran@minio.io>
2023-10-26 17:23:45 -0400

    Add `idp ldap accesskey` (#4686)

--- cmd/auto-complete.go
+++ cmd/auto-complete.go
@@ -381,6 +381,11 @@ var completeCmds = map[string]complete.Predictor{
 	"/idp/ldap/policy/attach":   aliasCompleter,
 	"/idp/ldap/policy/detach":   aliasCompleter,
 
+	"/idp/ldap/accesskey/create": aliasCompleter,
+	"/idp/ldap/accesskey/list":   aliasCompleter,
+	"/idp/ldap/accesskey/remove": aliasCompleter,
+	"/idp/ldap/accesskey/info":   aliasCompleter,
+
 	"/admin/policy/info":     aliasCompleter,
 	"/admin/policy/update":   aliasCompleter,
 	"/admin/policy/add":      aliasCompleter,
