bc47cebd91aa4ab695868b6b439d74f5b2716a79
Shubhendu <shubhendu@minio.io>
2024-04-19 14:22:20 +0530

    Show ILM Expiry Replication status only if enabled (#4900)
    In case Replication of ILM Expiry rules is disabled, mention the same
    in `mc admin replicate status ALIAS` output.
    
    Signed-off-by: Shubhendu Ram Tripathi <shubhendu@minio.io>

--- cmd/admin-replicate-status.go
+++ cmd/admin-replicate-status.go
@@ -326,6 +326,8 @@ func (i srStatus) String() string {
 		switch {
 		case i.MaxILMExpiryRules == 0:
 			messages = append(messages, console.Colorize("Summary", "No ILM Expiry Rules present\n"))
+		case i.ILMExpiryStats == nil:
+			messages = append(messages, console.Colorize("Summary", "Replication of ILM Expiry is not enabled\n"))
 		default:
 			msg := console.Colorize(i.getTheme(len(info.ILMExpiryStats) == 0), fmt.Sprintf("%d/%d ILM Expiry Rules in sync", info.MaxILMExpiryRules-len(info.ILMExpiryStats), info.MaxILMExpiryRules)) + "\n"
 			messages = append(messages, fmt.Sprintf("%s  %s", coloredDot, msg))
