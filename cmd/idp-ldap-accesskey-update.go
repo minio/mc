8e5995d770a9a30372a42f1f67dbfa41167f6a28
Taran <tpelkey2021@gmail.com>
2024-08-05 10:14:03 -0900

    new command

---
+++ cmd/idp-ldap-accesskey-update.go
@@ -0,0 +1,174 @@
+// Copyright (c) 2015-2024 MinIO, Inc.
+//
+// This file is part of MinIO Object Storage stack
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package cmd
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/minio/cli"
+	"github.com/minio/madmin-go/v3"
+	"github.com/minio/mc/pkg/probe"
+	"github.com/minio/pkg/v3/policy"
+)
+
+var idpLdapAccesskeyEditFlags = []cli.Flag{
+	cli.StringFlag{
+		Name:  "secret-key",
+		Usage: "set a secret key for the  account",
+	},
+	cli.StringFlag{
+		Name:  "policy",
+		Usage: "path to a JSON policy file",
+	},
+	cli.StringFlag{
+		Name:  "name",
+		Usage: "friendly name for the account",
+	},
+	cli.StringFlag{
+		Name:  "description",
+		Usage: "description for the account",
+	},
+	cli.StringFlag{
+		Name:  "expiry-duration",
+		Usage: "duration before the access key expires",
+	},
+	cli.StringFlag{
+		Name:  "expiry",
+		Usage: "expiry date for the access key",
+	},
+}
+
+var idpLdapAccesskeyEditCmd = cli.Command{
+	Name:         "edit",
+	Usage:        "edit existing access keys for LDAP",
+	Action:       mainIDPLdapAccesskeyEdit,
+	Before:       setGlobalsFromContext,
+	Flags:        append(idpLdapAccesskeyEditFlags, globalFlags...),
+	OnUsageError: onUsageError,
+	CustomHelpTemplate: `NAME:
+  {{.HelpName}} - {{.Usage}}
+
+USAGE:
+  {{.HelpName}} [FLAGS] [TARGET]
+
+FLAGS:
+  {{range .VisibleFlags}}{{.}}
+  {{end}}
+EXAMPLES:
+  TODO
+`,
+}
+
+func mainIDPLdapAccesskeyEdit(ctx *cli.Context) error {
+	if len(ctx.Args()) == 0 || len(ctx.Args()) > 2 {
+		showCommandHelpAndExit(ctx, 1) // last argument is exit code
+	}
+
+	args := ctx.Args()
+	aliasedURL := args.Get(0)
+	targetUser := args.Get(1)
+
+	if ctx.Bool("login") {
+		deprecatedError("mc idp ldap accesskey create-with-login")
+	}
+
+	opts := accessKeyUpdateOpts(ctx, targetUser)
+	client, err := newAdminClient(aliasedURL)
+	fatalIf(err, "Unable to initialize admin connection.")
+
+	res, e := client.AddServiceAccountLDAP(globalContext, opts)
+	fatalIf(probe.NewError(e), "Unable to add service account.")
+
+	m := ldapAccesskeyMessage{
+		op:          "create",
+		Status:      "success",
+		AccessKey:   res.AccessKey,
+		SecretKey:   res.SecretKey,
+		Expiration:  &res.Expiration,
+		Name:        opts.Name,
+		Description: opts.Description,
+	}
+	printMsg(m)
+
+	return nil
+}
+
+func accessKeyEditOpts(ctx *cli.Context, targetUser string) madmin.UpdateServiceAccountReq {
+	name := ctx.String("name")
+	expVal := ctx.String("expiry")
+	policyPath := ctx.String("policy")
+	secretKey := ctx.String("secret-key")
+	description := ctx.String("description")
+	expDurVal := ctx.Duration("expiry-duration")
+
+	if expVal != "" && expDurVal != 0 {
+		fatalIf(probe.NewError(errors.New("Only one of --expiry or --expiry-duration can be specified")), "invalid flags")
+	}
+
+	opts := madmin.UpdateServiceAccountReq{
+		TargetUser:  targetUser,
+		AccessKey:   accessKey,
+		SecretKey:   secretKey,
+		Name:        name,
+		Description: description,
+	}
+
+	if policyPath != "" {
+		// Validate the policy document and ensure it has at least one statement
+		policyBytes, e := os.ReadFile(policyPath)
+		fatalIf(probe.NewError(e), "unable to read the policy document")
+
+		p, e := policy.ParseConfig(bytes.NewReader(policyBytes))
+		fatalIf(probe.NewError(e), "unable to parse the policy document")
+
+		if p.IsEmpty() {
+			fatalIf(errInvalidArgument(), "empty policies are not allowed")
+		}
+
+		opts.Policy = policyBytes
+	}
+
+	switch {
+	case expVal != "":
+		location, e := time.LoadLocation("Local")
+		fatalIf(probe.NewError(e), "unable to load local location. verify your local TZ=<val> settings")
+
+		var found bool
+		for _, format := range supportedTimeFormats {
+			t, e := time.ParseInLocation(format, expVal, location)
+			if e == nil {
+				found = true
+				opts.Expiration = &t
+				break
+			}
+		}
+
+		if !found {
+			fatalIf(probe.NewError(fmt.Errorf("invalid expiry date format '%s'", expVal)), "unable to parse the expiry argument")
+		}
+	case expDurVal != 0:
+		t := time.Now().Add(expDurVal)
+		opts.Expiration = &t
+	}
+
+	return opts
+}
