4ca10b6d8748909d578334af0557a4410e012640
Allan Reid <allanrogerreid@gmail.com>
2024-11-20 03:39:51 -0400

    Add --with-delete-marker flag to toggle the listing of delete markers. Defaults to false

--- cmd/find.go
+++ cmd/find.go
@@ -274,7 +274,7 @@ func doFind(ctxCtx context.Context, ctx *findContext) error {
 
 	lstOptions := ListOptions{
 		WithOlderVersions: ctx.withVersions,
-		WithDeleteMarkers: true,
+		WithDeleteMarkers: ctx.withDeleteMarkers,
 		Recursive:         true,
 		ShowDir:           DirFirst,
 		WithMetadata:      len(ctx.matchMeta) > 0 || len(ctx.matchTags) > 0,
